ca5e544684afc88e3d30885600fadb4a03cf7ef6
Antoine DEPAIX <32707955+Nogao@users.noreply.github.com>
2018-04-12 21:41:51 +0200

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<header class="Header">
+    <div class="Header-top">
+    	<ul class="Header-Langues">
+    		<li class="Header-Langues-item-fr">FR</li>
+    		<li class="Header-Langues-item">EN</li>
+    		<li class="Header-Langues-item">ES</li>
+    	</ul>
+    	<div class="Header-Bonjour">BONJOUR LOUISE <a class="Header-Link" href="#">SE DECONNECTER</a><img class="Header-Bonjour-img" src="img/panier.png"></div>
+
+    </div>
+    <div class="Header-bottom">
+    	<h1 class="Header-bottom-Title">sokaris</h1>
+        <nav>
+            <ul class="Header-bottom-Liste">
+                <li class="Header-bottom-Liste-item">
+                <a class="Header-bottom-Liste-item-link" href="#">CONFIGURER VOTRE BIJOU</a></li>
+                <li class="Header-bottom-Liste-item">
+                <a class="Header-bottom-Liste-item-link" href="#">LES MODELES DU MOIS</a></li>
+                <li class="Header-bottom-Liste-item">
+                <a class="Header-bottom-Liste-item-link" href="#">LES MODELES DE CREATEURS</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
